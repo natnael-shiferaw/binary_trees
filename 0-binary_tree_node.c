1e05f56059cd7403337c13ee510d38e6a6f6bb3f
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-01-03 19:30:26 +0300

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-if (!newNode)
+if(newNode == NULL)
 {
-free(newNode);
 return (NULL);
 }
 newNode->n = value;
