ba0e72bc2901dc559dac291e9e6b9b000641f0de
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-01-03 19:47:18 +0300

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,9 +9,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-binary_tree_t *newNode = binary_tree_node(parent, value);
+binary_tree_t *newNode;
+if (parent == NULL)
+    return (NULL);
+newNode = binary_tree_node(parent, value);
 
-if (newNode == NULL || parent == NULL)
+if (newNode == NULL)
 {
 return (NULL);
 }
