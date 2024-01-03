aa7fe48477e3c2f58519bd8cbfc0404f367ad184
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-01-03 21:51:40 +0300

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,12 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_height, right_height;
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
 if (tree == NULL)
 {
 	return (0);
 }
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
 
 if (left_height >= right_height)
 	return (left_height + 1);
