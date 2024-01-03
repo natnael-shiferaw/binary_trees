d734056aa90b7226425e6f6f3822c2de1b310373
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-01-03 19:41:12 +0300

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,21 +11,21 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *newNode = binary_tree_node(parent, value);
 
-if(newNode == NULL || parent == NULL)
+if (newNode == NULL || parent == NULL)
 {
-return NULL;
+return (NULL);
 }
 
-if(parent->left != NULL)
+if (parent->left != NULL)
 {
 /*old left child*/
-binary_tree_t* prevNode = parent->left;
+binary_tree_t *prevNode = parent->left;
 /*setting old left child to new node*/
 newNode->left = prevNode;
-prevNode->parent =newNode;
+prevNode->parent = newNode;
 }
 /*inserting at the left*/
 parent->left = newNode;
 
-return newNode;
+return (newNode);
 }
