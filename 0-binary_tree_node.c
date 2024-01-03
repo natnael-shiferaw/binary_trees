4ac21c4e46b113766c9da7e55c4afa7bb27eb309
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-01-03 19:19:19 +0300

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,20 +4,22 @@
 * binary_tree_node - creates a binary tree node
 * @parent: pointer to a parent node
 * @value: value to put in the new node
+* Return: pointer to the created node, NULL on failure
 */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-binary_tree_t* newNode = malloc(sizeof(binary_tree_t));
-
-if(!newNode){
-    free(newNode);
-    return NULL; //checking whether malloc was successfull
+if(!newNode)
+{
+free(newNode);
+return (NULL);
 }
 newNode->n = value;
 newNode->parent = parent;
 newNode->left = NULL;
 newNode->right = NULL;
 
-return newNode;
+return (newNode);
 }
